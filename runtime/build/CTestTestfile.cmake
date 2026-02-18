bbebf6f8942f8833fe8c9c80e5e52931ef0098f9
siridech-bo <siridech.bo@kmitl.ac.th>
2026-02-18 21:55:41 +0700

    build with NCNN

--- runtime/build/CTestTestfile.cmake
+++ runtime/build/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(test_ncnn "D:/CiRA Claw/cira-edge/runtime/build/test_ncnn.exe")
+set_tests_properties(test_ncnn PROPERTIES  _BACKTRACE_TRIPLES "D:/CiRA Claw/cira-edge/runtime/CMakeLists.txt;196;add_test;D:/CiRA Claw/cira-edge/runtime/CMakeLists.txt;0;")
